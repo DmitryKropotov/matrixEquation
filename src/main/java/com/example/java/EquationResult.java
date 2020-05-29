6c49de005b1bc41ead39a899fa8b1114c5dc1f69
kropotovd <kropotovd@gmail.com>
2020-05-29 22:10:53 +0300

    Made for some simple cases

--- src/main/java/com/example/java/EquationResult.java
+++ src/main/java/com/example/java/EquationResult.java
@@ -4,10 +4,28 @@ import lombok.Data;
 
 @Data
 public class EquationResult {
-    private double[][] vectors;
+    private double[][] vectors = new  double[0][0];
     private boolean noDecisions = false;
 
     public int getVectorSize() {
         return vectors.length;
+    }
+
+    @Override
+    public String toString() {
+       if (noDecisions) {
+           return "The system has no decisions";
+       } else if (vectors.length == 0) {
+           return "trivial decision";
+       } else {
+           StringBuilder result = new StringBuilder();
+           for (double[] vector : vectors) {
+               for (int i = 0; i < vector.length; i++) {
+                   result.append(vector[i]+" ");
+               }
+               result.append("\\");
+           }
+           return result.toString();
+       }
     }
 }
--- src/main/java/com/example/java/Main.java
+++ src/main/java/com/example/java/Main.java
@@ -16,8 +16,8 @@ public class Main {
 
         List<double[]> s = new ArrayList();
 
-        String nextNumbers = scanner.next();
-        int eqLength = 0;
+        String nextNumbers = scanner.nextLine();
+        int eqLength = -1;
         do {
             String[] numbers = nextNumbers.split(" ");
             if (eqLength != 0) {
@@ -32,13 +32,15 @@ public class Main {
                 }
                 s.add(sItem);
             }
-            nextNumbers = scanner.next();
+            nextNumbers = scanner.nextLine();
+            System.out.println(nextNumbers + " " + nextNumbers.equals(""));
         } while (!nextNumbers.equals(""));
         double[][] arrayS = new double[s.size()][eqLength];
         for (int i = 0; i < s.size(); i++) {
             arrayS[i] = s.get(i);
         }
-        solveSystem(arrayS);
+        EquationResult result = solveSystem(arrayS);
+        System.out.println(result);
     }
 
     private static EquationResult solveSystem(double[][] s) {
@@ -54,6 +56,21 @@ public class Main {
         if (s[0].length>s.length) {
             final int varEquationsDif =  s[0].length-s.length;
             double[][] vectors = new double[varEquationsDif][s[0].length];
+            for (int i = s.length-1; i>=0; i--) {
+                double[] coefficients = new double[s[i].length];
+                for (int j = 0; j < s[i].length; j++) {
+                   coefficients[j] += -s[i][j]/s[i][i];
+                   if (j < s.length) {
+                       vectors[i][j] = coefficients[j];
+                   } else if (j-i == s.length) {
+                       vectors[i][j] = 1;
+                   }
+                }
+                s = replaceVarFromEqInOthers(s, i, i, coefficients);
+            }
+            result.setVectors(vectors);
+
+           /* double[][] vectors = new double[varEquationsDif][s[0].length];
             for (int i = 0; i < vectors.length; i++) {
                 double[] vector = vectors[i];
                 vector[i+s[0].length] = 1;
@@ -61,9 +78,32 @@ public class Main {
             for (int i = varEquationsDif-1; i >= 0; i--) {
                 s[i][i] =
             }
-            result.setVectors(vectors);
+            result.setVectors(vectors);*/
         }  else if (s[0].length<s.length) {
             result.setNoDecisions(true);
+        }
+        return result;
+    }
+
+    private static double[][] replaceVarFromEqInOthers(double[][] s, int eqNumber, int varNumber, double[] coefficients) {
+        if (s.length == 0 || eqNumber >= s.length || varNumber >= s[0].length) {
+            throw new IllegalArgumentException();
+        }
+        /*double[] coefficients = new double[s[eqNumber].length];
+        for (int i = 0; i < s[eqNumber].length; i++) {
+            coefficients[i] += -s[eqNumber][i]/s[eqNumber][varNumber];
+        }*/
+        double[][] result = new double[s.length-1][s[0].length-1];
+        for (int i = 0; i < s.length; i++) {
+            int newEqNumber = i < eqNumber? i: i-1;
+            if (i != eqNumber) {
+                for (int j = 0; j < s[i].length; j++) {
+                    int newVarNumber = j < varNumber? j: j-1;
+                    if (j != varNumber) {
+                        result[newEqNumber][newVarNumber] = s[i][j] / coefficients[j];
+                    }
+                }
+            }
         }
         return result;
     }
